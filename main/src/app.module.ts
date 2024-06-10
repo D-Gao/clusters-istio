7ebb0b665f9d96e5d0770f455f9e3e110e5469e4
D-Gao <huang245415359@gmail.com>
2024-06-10 10:08:08 +0200

    update helm chart main

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService globally available
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- main/src/users/users.module.ts
+++ main/src/users/users.module.ts
@@ -4,18 +4,29 @@ import { UsersController } from './users.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { protobufPackage } from 'src/types/auth';
 import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'AUTH_SERVICE',
-        transport: Transport.GRPC,
+        /* transport: Transport.GRPC,
         options: {
           url: 'grpc-container:50051',
           package: protobufPackage,
           protoPath: join(__dirname, '../proto/auth.proto'),
-        },
+        }, */
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            url: configService.get<string>('GRPC_SERVER_URL'),
+            package: protobufPackage,
+            protoPath: join(__dirname, '../proto/auth.proto'),
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
